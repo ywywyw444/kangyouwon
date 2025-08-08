1bc986834e9770f81d4599b2b5bec5be784688fb
ywywyw444 <vjvm99@gmail.com>
2025-08-08 15:31:26 +0900

    25.08.08

--- gateway/run.py
+++ gateway/run.py
@@ -8,7 +8,7 @@ if os.getenv("RAILWAY_ENVIRONMENT") != "true":
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.router.main:app",
+        "app.main:app",
         host=os.getenv("GATEWAY_HOST", "0.0.0.0"),
         port=int(os.getenv("GATEWAY_PORT", 8080)),
         reload=os.getenv("GATEWAY_RELOAD", "false").lower() == "true",
